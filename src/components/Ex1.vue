5f0c243301bd6f806594e52f19319b137fa1d9a5
adriantanxy <adrian.tan.2024@computing.smu.edu.sg>
2025-10-21 05:16:33 +0000

    correct q1

--- src/components/Ex1.vue
+++ src/components/Ex1.vue
@@ -2,6 +2,46 @@
     export default { 
         // Add Code Here to complete the task
         // Note: DO NOT USE "eval()". In security, "eval" is considered "evil"!!!
+        data(){
+            return{
+                operators: [
+                    '+',
+                    '*',
+                    '-',
+                    '/',
+                    '%'
+                ],
+                x: 0,
+                y: 0,
+                selectedOp: '',
+            }
+        },
+        computed: {
+            result(){
+                // if(this.selectedOp == "+"){
+                //     return this.x + this.y
+                // }
+                // if(this.selectedOp == "-"){
+                //     return this.x - this.y
+                // }
+                // if(this.selectedOp == "*"){
+                //     return this.x * this.y
+                // }
+                // if(this.selectedOp == "/"){
+                //     return this.x / this.y
+                // }
+                // if(this.selectedOp == "%"){
+                //     return this.x % this.y
+                // }
+                switch(this.selectedOp){
+                    case "+": return this.x + this.y;
+                    case "/": return this.x / this.y;
+                    case "-": return this.x - this.y;
+                    case "*": return this.x * this.y;
+                    case "%": return this.x % this.y;
+                }
+            }
+        }
     }
 </script>
 
