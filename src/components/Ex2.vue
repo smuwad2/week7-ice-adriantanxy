dc920948f1a343e902ff8b89878339fbc17f73c3
adriantanxy <adrian.tan.2024@computing.smu.edu.sg>
2025-10-21 06:02:54 +0000

    correct ex2

--- src/components/Ex2.vue
+++ src/components/Ex2.vue
@@ -3,9 +3,15 @@
         // Add code in appropriate places
         data() {
             return {
-                items: ['keyboard', 'mouse', 'iPhone', 'macbook', 'adapter']
+                items: ['keyboard', 'mouse', 'iPhone', 'macbook', 'adapter'],
+                newItem: '',
             }
-            
+        },
+        methods: {
+            addItem(){
+                //add the new item to the items array
+                this.items.push(this.newItem);
+            }
         }
     }
 </script>
@@ -14,5 +20,13 @@
 
     <h2>Shopping Cart</h2>
     <!-- TODO: Add Code Here -->
+     <ul>
+        <li v-for="(item, idx) in items">{{ item }}
+            <button class="bg-light text-dark" @click="items.splice(idx,1)">Delete!</button>
+        </li>
+     </ul>
+     
+     <input type="text" v-model="newItem"></input>
+     <button @click="addItem">Add!</button>
    
 </template>
